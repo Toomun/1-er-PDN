f73b6e3c0de8f3a04142c17be067a1f04258c655
RamiroVeloz <112987903+RamiroVeloz@users.noreply.github.com>
2023-05-29 22:28:59 -0300

    Add files via upload

---
+++ css/js/custom.js
@@ -0,0 +1,49 @@
+//Boton de menu hamburguesa
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click",() => {
+    nav.classList.remove("visible")
+})
+
+
+//Carrousel de imagenes del banner
+
+const grande    = document.querySelector('.grande')
+const punto     = document.querySelectorAll('.punto')
+
+// Cuando CLICK en punto
+    // Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // QUITAR la clase activo de TODOS puntos
+    // AÑADIR la clase activo al punto que hemos hecho CLICK
+
+// Recorrer TODOS los punto
+punto.forEach( ( cadaPunto , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -50
+
+        // MOVEMOS el grand
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+
+    })
+})
---
+++ css/js/noticias.js
@@ -0,0 +1,72 @@
+let pagina = 0;
+let temaPrincipal = 'all' ;
+const cargarNoticias = async (categoria) => {
+    let url = `https://inshorts.deta.dev/news?category=${categoria}`
+    try{
+        const respuesta = await fetch (url);
+        console.log(respuesta)
+
+        if(respuesta.status === 200){
+            const datos = await respuesta.json();
+            console.log(datos)
+
+            let noticias = '';
+            datos.data.forEach(noticia =>{
+                noticias += `<a href ='${noticia.readMoreUrl}'>
+                            <div class ="item">
+                            <h3 class ="titnot">${noticia.title}</h3>
+                            <img class="imgnot " src="${noticia.imageUrl}">
+                            <h5 class="datenot">${noticia.date}</h5>
+                            </div>
+                            </a>`;
+                
+            })
+            document.getElementById('container-noticias').innerHTML = noticias;
+            
+
+
+        } else if (respuesta.status === 401){
+            console.log('Error')
+        } else if (respuesta.status === 404){
+            console.log ('La noticia no existe')
+        }else{
+            console.log ('Error inesperado.')
+        }
+
+
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+        
+
+
+
+
+
+function buscar(cat){
+    pagina = 0;
+    temaActual = cat;
+    cargarNoticias(cat);
+}
+
+function buscarTema(){
+    pagina =0;
+    let tema = document.querySelector("#busqueda").value;
+    temaActual = tema;
+    cargarNoticias(temaActual);
+}
+
+function detectarEnter(){
+    var input = document.getElementById('busqueda');
+    input.addEventListener('keypress',function(e){
+        if (e.key === 'Enter'){
+            document.getElementById('btn-buscar').click();
+        }
+    })
+}
+
+
+cargarNoticias(temaPrincipal);
